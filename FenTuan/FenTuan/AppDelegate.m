4812c886d4373facb5d214f4fc9d28aab83aa6d0
zhufeng <358244144@qq.com>
2018-06-30 11:35:38 -0700

    提交下bugtags的管理工具的使用

--- FenTuan/FenTuan/AppDelegate.m
+++ FenTuan/FenTuan/AppDelegate.m
@@ -11,6 +11,7 @@
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    ///初始化三方的东西
     self.window = [[UIWindow alloc]initWithFrame:[UIScreen mainScreen].bounds];
     self.window.backgroundColor = [UIColor whiteColor];
     FTLoginVC *loginvc = [[FTLoginVC alloc]init];
@@ -18,12 +19,11 @@
     [self.window makeKeyAndVisible];
     return YES;
 }
+
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
 }
-
-
 - (void)applicationDidEnterBackground:(UIApplication *)application {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
